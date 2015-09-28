1cbf1b2880ba28e3fc31584f32dc9ac15a5dcba5
Alex Davis <davis.alex.jr@gmail.com>
2015-09-28 13:57:14 -0400

    Added ability to add items

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
-    // YOUR CODE HERE!
+    $(".add-items").submit(function(event){
+    	event.preventDefault();
+
+    	var item = $("#todo-list-item").val();
+
+    	if (item) {
+    		$("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>")
+    		$("#todo-list-item").val("");
+    	};
+    	
+    });
+
+    // $(".checkbox").change(function(){
+    // 	console.log("Checkbox changed");
+    // })
+
+	$(document).on("change", ".checkbox", function(){
+		
+	});
+
 });
