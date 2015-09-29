03024754d7595cfdb29dc1bac2f111d58efba4cb
Alex Davis <davis.alex.jr@gmail.com>
2015-09-29 11:11:26 -0400

    Added local storage to save todo items and thier current states.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+
+    //show items saved in local storage
+    $("#list-items").html(localStorage.getItem("listItems"));
+
+    //Create new todo items
     $(".add-items").submit(function(event){
     	event.preventDefault();
 
@@ -6,17 +11,30 @@ $(document).ready(function () {
 
     	if (item) {
     		$("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>")
+            localStorage.setItem("listItems", $("#list-items").html());
     		$("#todo-list-item").val("");
     	};
     	
     });
 
-    // $(".checkbox").change(function(){
-    // 	console.log("Checkbox changed");
-    // })
-
+    //check to see if item is checked, if true remove checked
 	$(document).on("change", ".checkbox", function(){
-		
+        if($(this).attr("checked")){
+            $(this).removeAttr("checked");
+        }
+        else{
+            $(this).attr("checked", "checked");    
+        }
+        
+        //toggle the checkbox with "completed" class
+		$(this).parent().toggleClass("completed");
+        localStorage.setItem("listItems", $("#list-items").html());
 	});
+
+    //remove item when clicked
+    $(document).on("click", ".remove", function(){
+        $(this).parent().remove();
+        localStorage.setItem("listItems", $("#list-items").html());
+    });
 
 });
